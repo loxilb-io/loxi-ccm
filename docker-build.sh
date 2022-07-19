706485b8c5e1b2c45f7b861f0f552bc4f63f1990
backguynn <backguyn@netlox.io>
2022-07-19 10:59:45 +0900

    test gitignore

--- docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tag="0.1"
-
-echo "building loxi-ccm docker image..."
-sudo docker build -t eyes852/loxi-ccm:$tag -f ./Dockerfile .
-echo "uploading loxi-ccm docker image to docker hub..."
-sudo docker push eyes852/loxi-ccm:$tag
